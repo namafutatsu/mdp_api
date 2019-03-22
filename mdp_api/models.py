2f8616108493936886490552bc85a0f1e03a0243
Victor Perron <victor@iso3103.net>
2019-03-22 14:40:15 +0100

    Users: add newsletter & shop

--- mdp_api/models.py
+++ mdp_api/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     avatar = models.ImageField(blank=True, null=True)
+    has_newsletter = models.BooleanField(default=True)
+    shop = models.ForeignKey('mdp_api_api.Shop', blank=True, null=True, related_name='users', on_delete=models.PROTECT)
