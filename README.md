9717ca49af07651ec3d49d9fcef452a45647a7d0
Victor Perron <victor@iso3103.net>
2019-03-22 15:04:36 +0100

    scripts: Inject every user as well.

--- README.md
+++ README.md
@@ -6,6 +6,25 @@ Needs django-newsletter and a fork of the django-places app.
 
 Get rollin' baby.
 
+# Injection
+
+(mdp_api) [victor@M]<~/perso/mdp-api> ENVIRONMENT=dev python inject.py scripts/data/magasindbhmdprod_table_mv_region.csv scripts/mv_reseau.csv scripts/mv_magasin.csv scripts/data/magasindbhmdprod_table_mv_user.csv
+/home/victor/.virtualenvs/mdp_api/lib/python3.6/site-packages/psycopg2/__init__.py:144: UserWarning: The psycopg2 wheel package will be renamed from release 2.8; in order to keep installing from binary please use "pip install psycopg2-binary" instead. For details see: <http://initd.org/psycopg/docs/install.html#binary-install-from-pypi>.
+  """)
+>>>>>>> error: shop=Chèvrefeuille region=0 does not exist
+>>>>>>> error: shop=Chez Mon Fermier region=0 does not exist
+>>>>>>> error: shop=Au Panier des 7 Vallées region=0 does not exist
+>>>>>>> error: shop=Bio d&#146;Ici region=0 does not exist
+>>>>>>> error: shop=Bio Terroir region=0 does not exist
+>>>>>>> error: shop=La Boutique des Fermiers region=0 does not exist
+>>>>>>> error: shop=La Clef des Champs region=0 does not exist
+>>>>>>> error: shop=La Corbeille Paysanne region=0 does not exist
+>>>>>>> error: user=sarlbioterroir@orange.fr shop=102 does not exist
+>>>>>>> error: user=villa.leona@yahoo.com shop=147 does not exist
+>>>>>>> error: username=Gagephiko1991 email=mmflgp@gmail.com already exists
+>>>>>>> error: user=contact@cooppaysanne.fr shop=335 does not exist
+>>>>>>> error: username=Luhei email=heisslerlucette@yahoo.fr already exists
+
 ## LICENSE
 
 MIT
--- scripts/inject.py
+++ scripts/inject.py
@@ -7,6 +7,7 @@ import os
 from os import path
 
 import django
+import django.db.utils
 from django.core.files import File
 from django.contrib.gis.geos import Point
 
@@ -15,6 +16,7 @@ os.environ.setdefault('SECRET_KEY', 'caca')
 
 django.setup()
 
+from mdp_api.models import User
 from mdp_api.api import enums
 from mdp_api.api.models import Shop, ShopNetwork, ShopRegion
 
@@ -37,8 +39,10 @@ if __name__ == '__main__':
     parser.add_argument('region_filename', metavar='REGION_FILE NAME', help='the input regions CSV filename')
     parser.add_argument('network_filename', metavar='NETWORK_FILE NAME', help='the input networks CSV filename')
     parser.add_argument('shops_filename', metavar='SHOPS_FILE NAME', help='the input shops CSV filename')
+    parser.add_argument('users_filename', metavar='USERS_FILE NAME', help='the input users CSV filename')
     args = parser.parse_args()
 
+    User.objects.all().delete()
     Shop.objects.all().delete()
     ShopRegion.objects.all().delete()
     ShopNetwork.objects.all().delete()
@@ -51,7 +55,6 @@ if __name__ == '__main__':
             description=clean(dct['reg_desc']),
             coords=Point(float(dct['reg_long']), float(dct['reg_lat'])),
         )
-
 
     for dct in read_csv_data(args.network_filename):
         ShopNetwork.objects.create(
@@ -104,19 +107,28 @@ if __name__ == '__main__':
         img_name = 'shop_%s.png' % int(dct['m_id'])
         with open('scripts/shop-images/%s' % img_name, 'rb') as f:
             shop.picture.save("%s.png" % dct['m_id'], File(f))
-    import os; os._exit(0)
 
-    places = [ShopData(**dct) for dct in read_csv_data(args.shops_filename)]
-    Shop.objects.all().delete()
-    for place in places:
+    for dct in read_csv_data(args.users_filename):
+        # "u_id","u_mail","u_mail_valid","u_pwd","u_valid","u_type","u_magid","u_tmp_magid","u_annee","u_sexe","u_pseudo","u_cp","u_visites","u_last_visit","u_creation","u_key","u_prod_fiche","u_tel"
         try:
-            Location.objects.create(name=place.name, address=place.address, coords=Point(place.lng, place.lat))
-        except django.db.utils.IntegrityError as exc:
-            print(">>>> ERROR: location '{}' already exists".format(place.name))
-
-    Organization.objects.all().delete()
-    for org in read_csv_data(args.organizations_filename):
-        try:
-            Organization.objects.create(name=org[0])
-        except django.db.utils.IntegrityError as exc:
-            print(">>>> ERROR: organization '{}' already exists".format(org[0]))
+            email = clean(dct['u_mail'])
+            username = clean(dct['u_pseudo'])
+            user = User.objects.create(
+                pk=int(dct['u_id']),
+                email=email,
+                username=username,
+                has_newsletter=bool(dct['u_mail_valid']),
+            )
+        except django.db.utils.IntegrityError:
+            print(">>>>>>> error: username=%s email=%s already exists" % (username, email))
+            continue
+        user.set_password(dct['u_pwd'])
+        shop_id = int(dct['u_magid']) or int(dct['u_tmp_magid'])
+        if shop_id != 0:
+            try:
+                shop = Shop.objects.get(pk=shop_id)
+                user.shop = shop
+            except (ValueError, Shop.DoesNotExist):
+                print(">>>>>>> error: user=%s shop=%s does not exist" % (user.email, shop_id))
+                continue
+        user.save()
