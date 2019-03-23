047132ae8e799d734f87d7d684329d2028bb54dc
Victor Perron <victor@iso3103.net>
2019-03-23 13:34:23 +0100

    urls: Fix path deprecated issue

--- mdp_api/urls.py
+++ mdp_api/urls.py
@@ -17,10 +17,10 @@ urlpatterns = [
     path('api/v0/overview/', views.Overview.as_view(), name='overview'),
 
     # Account related views
-    path('account/profile/$', views.CurrentUserView.as_view(), name='profile'),
+    path('account/profile/', views.CurrentUserView.as_view(), name='profile'),
     path('account/token/', rest_framework.authtoken.views.obtain_auth_token),
-    path('account/signup/$', account_views.signup, name='signup'),
-    path('account/json_signup/$', account_views.json_signup, name='json_signup'),
+    path('account/signup/', account_views.signup, name='signup'),
+    path('account/json_signup/', account_views.json_signup, name='json_signup'),
 
     # WWW views
     path('admin/', admin.site.urls, name='admin'),
